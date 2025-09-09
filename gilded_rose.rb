e3f22fc086f59e5570816833ce231563489a9222
Petteri Kettunen <ptrktn@users.noreply.github.com>
2025-09-09 19:52:52 +0900

    Refactor update

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,58 +1,111 @@
 class GildedRose
+  class InvalidInput < StandardError; end
+  class InvalidItem < StandardError; end
 
   def initialize(items)
-    @items = items
+    raise InvalidInput unless items.is_a?(Array)
+    raise InvalidItem unless items.all? { |item| item.instance_of?(Item) }
+
+    @items = items # Do not modify.
   end
 
-  def update_quality()
+  def update_quality
     @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
+      ItemTransformer.transform(item).update
     end
   end
 end
 
+module ItemTransformer
+  def self.transform(item)
+    name, conjured =
+      if item.name.split[0] == 'Conjured'
+        [item.name.split[1..].join(' '), true]
+      else
+        [item.name, false]
+      end
+
+    if name.start_with?('Aged')
+      AgedItem.new(item, conjured:)
+    elsif name.start_with?('Backstage passes')
+      AdmissionItem.new(item, conjured:)
+    elsif name.start_with?('Sulfuras')
+      LegendaryItem.new(item, conjured:)
+    else
+      BaseItem.new(item, conjured:)
+    end
+  end
+end
+
+class BaseItem
+  def initialize(item, conjured: false)
+    @item = item
+    @conjured = conjured
+  end
+
+  def update
+    amount = @item.sell_in.positive? ? 1 : 2
+    amount *= 2 if @conjured
+    decrease_quality(amount)
+    decrease_sell_in
+  end
+
+  private
+
+  def decrease_sell_in
+    @item.sell_in -= 1
+  end
+
+  def decrease_quality(amount = 1)
+    @item.quality = [@item.quality - amount, 0].max
+  end
+
+  def increase_quality(amount = 1)
+    @item.quality = [@item.quality + amount, 50].min
+  end
+end
+
+class AgedItem < BaseItem
+  def update
+    increase_quality(@conjured ? 2 : 1)
+    decrease_sell_in
+    increase_quality(@conjured ? 2 : 1) if @item.sell_in.negative?
+  end
+end
+
+class LegendaryItem < BaseItem
+  class InvalidQuality < StandardError; end
+
+  def initialize(item, conjured: false)
+    raise InvalidQuality unless item.quality == 80
+
+    super(item, conjured:)
+  end
+
+  def update
+    # Quality and sell_in do not change, nothing to do here.
+  end
+end
+
+class AdmissionItem < BaseItem
+  def update
+    amount =
+      if @item.sell_in < 6
+        3
+      elsif @item.sell_in < 11
+        2
+      else
+        1
+      end
+
+    amount *= 2 if @conjured
+    increase_quality(amount)
+    decrease_sell_in
+    @item.quality = 0 if @item.sell_in.negative?
+  end
+end
+
+# Do not modify this class.
 class Item
   attr_accessor :name, :sell_in, :quality
 
--- gilded_rose_spec.rb
+++ gilded_rose_spec.rb
@@ -1,11 +1,268 @@
-require 'rspec'
+# frozen_string_literal: true
 
 require File.join(File.dirname(__FILE__), 'gilded_rose')
 
 describe GildedRose do
-  it "does not change the name" do
-    items = [Item.new("foo", 0, 0)]
-    GildedRose.new(items).update_quality()
-    expect(items[0].name).to eq "fixme"
+  describe '#update_quality' do
+    subject { GildedRose.new(items).update_quality }
+    let(:name) { 'foo' }
+    let(:sell_in) { 10 }
+    let(:quality) { 10 }
+    let(:item) { Item.new(name, sell_in, quality) }
+    let(:items) { [item] }
+
+    shared_examples 'name is preserved and sell_in decreases' do
+      it 'name is preserved' do
+        subject
+        expect(items[0].name).to eq(name)
+      end
+
+      it 'sell_in decreases by one' do
+        subject
+        expect(items[0].sell_in).to eq(sell_in - 1)
+      end
+    end
+
+    describe 'When the input is not an array' do
+      let(:items) { nil }
+
+      it 'throws a GildedRose::UnsupportedInput error' do
+        # expect { GildedRose.new(nil).update_quality }.to raise_error(GildedRose::UnsupportedInput)
+        expect { subject }.to raise_error(GildedRose::InvalidInput)
+      end
+    end
+
+    describe 'When an element in the input is of invalid type' do
+      let(:item) { nil }
+
+      it 'throws a GildedRose::UnsupportedItem error' do
+        expect { subject }.to raise_error(GildedRose::InvalidItem)
+      end
+    end
+
+    context 'Other than Legendary items minimum quality' do
+      let(:quality) { 0 }
+
+      it 'is zero' do
+        subject
+        expect(items[0].quality).to eq(0)
+      end
+    end
+
+    context 'Other than Legendary items maximum quality' do
+      let(:quality) { 50 }
+      let(:name) { 'Aged Brie' } # increases by time
+
+      it 'is 50' do
+        subject
+        expect(items[0].quality).to eq(50)
+      end
+    end
+
+    context 'Ordinary item' do
+      it_behaves_like 'name is preserved and sell_in decreases'
+
+      context 'Before the sell_in date' do
+        it 'quality decreases by one' do
+          subject
+          expect(items[0].quality).to eq(quality - 1)
+        end
+      end
+
+      context 'After the sell_in date' do
+        let(:sell_in) { -1 }
+
+        it 'quality decreases twice as fast' do
+          subject
+          expect(items[0].quality).to eq(quality - 2)
+        end
+      end
+      context 'Conjured ordinary item' do
+        let(:name) { 'Conjured foo' }
+
+        context 'Before the sell_in date' do
+          it 'quality decreases by two' do
+            subject
+            expect(items[0].quality).to eq(quality - 2)
+          end
+        end
+
+        context 'After the sell_in date' do
+          let(:sell_in) { -1 }
+
+          it 'quality decreases twice as fast' do
+            subject
+            expect(items[0].quality).to eq(quality - 4)
+          end
+        end
+      end
+    end
+
+    context 'Aged item' do
+      let(:name) { 'Aged Brie' }
+
+      it_behaves_like 'name is preserved and sell_in decreases'
+
+      context 'Before the sell_in date' do
+        it 'quality increases by one' do
+          subject
+          expect(items[0].quality).to eq(quality + 1)
+        end
+      end
+
+      context 'After the sell_in date' do
+        let(:sell_in) { -1 }
+
+        it 'quality increases by two' do
+          subject
+          expect(items[0].quality).to eq(quality + 2)
+        end
+      end
+      context 'Conjured Aged item' do
+        let(:name) { 'Conjured Aged Brie' }
+
+        context 'Before the sell_in date' do
+          it 'increases by two' do
+            subject
+            expect(items[0].quality).to eq(quality + 2)
+          end
+        end
+
+        context 'After the sell_in date' do
+          let(:sell_in) { -1 }
+
+          it 'quality increases twice as fast' do
+            subject
+            expect(items[0].quality).to eq(quality + 4)
+          end
+        end
+      end
+    end
+
+    context 'Legendary item' do
+      let(:quality) { 80 }
+      let(:name) { 'Sulfuras, Hand of Ragnaros' }
+
+      context 'When quality is other than 80' do
+        let(:quality) { 10 }
+
+        it 'raises LegendaryItem::InvalidQuality' do
+          expect { subject }.to raise_error(LegendaryItem::InvalidQuality)
+        end
+      end
+
+      it 'sell_in does not change' do
+        subject
+        expect(items[0].sell_in).to eq(sell_in)
+      end
+
+      it 'name is preserved' do
+        subject
+        expect(items[0].name).to eq(name)
+      end
+
+      it 'quality is 80' do
+        subject
+        expect(items[0].quality).to eq(80)
+      end
+
+      context 'Conjured Legendary item' do
+        let(:quality) { 80 }
+        let(:name) { 'Conjured Sulfuras, Hand of Ragnaros' }
+
+        it 'sell_in does not change' do
+          subject
+          expect(items[0].sell_in).to eq(sell_in)
+        end
+
+        it 'quality is always 80' do
+          subject
+          expect(items[0].quality).to eq(80)
+        end
+      end
+    end
+
+    context 'Admission item' do
+      let(:name) { 'Backstage passes to a TAFKAL80ETC concert' }
+
+      it_behaves_like 'name is preserved and sell_in decreases'
+
+      context 'More than ten days before the event' do
+        let(:sell_in) { 20 }
+
+        it 'quality increases by one' do
+          subject
+          expect(items[0].quality).to eq(quality + 1)
+        end
+      end
+
+      context 'Ten days but more than five days before the event' do
+        let(:sell_in) { 9 }
+
+        it 'quality increases by two' do
+          subject
+          expect(items[0].quality).to eq(quality + 2)
+        end
+      end
+
+      context 'Five days or less before the event' do
+        let(:sell_in) { 4 }
+
+        it 'quality increases by three' do
+          subject
+          expect(items[0].quality).to eq(quality + 3)
+        end
+      end
+
+      context 'After the event' do
+        let(:sell_in) { -1 }
+
+        it 'quality is zero' do
+          subject
+
+          expect(items[0].quality).to eq(0)
+        end
+      end
+      context 'Conjured Admission item' do
+        let(:name) { 'Conjured Backstage passes to a TAFKAL80ETC concert' }
+
+        context 'More than ten days before the event' do
+          let(:sell_in) { 20 }
+
+          it 'quality increases by two' do
+            subject
+            expect(items[0].quality).to eq(quality + 2)
+          end
+        end
+
+        context 'Ten days but more than five days before the event' do
+          let(:sell_in) { 9 }
+
+          it 'quality increases by four' do
+            subject
+            expect(items[0].quality).to eq(quality + 4)
+          end
+        end
+
+        context 'Five days or less before the event' do
+          let(:sell_in) { 4 }
+
+          it 'quality increases by six' do
+            subject
+            expect(items[0].quality).to eq(quality + 6)
+          end
+        end
+
+        context 'After the event' do
+          let(:sell_in) { -1 }
+
+          it 'quality is zero' do
+            subject
+
+            expect(items[0].quality).to eq(0)
+          end
+        end
+      end
+    end
   end
 end
